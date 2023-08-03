320653716054c80e5c007991d7dde4d73c04e719
Vlad Khmelevskoi <vladyslav.khmelevskoi.wj@renesas.com>
2023-08-03 19:53:02 +0300

    Add cubeIDE projects

--- flasher_stm32/Core/Src/main.c
+++ flasher_stm32/Core/Src/main.c
@@ -60,6 +60,7 @@ UART_HandleTypeDef huart1;
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -283,31 +284,31 @@ uint32_t convertToInt(uint8_t bytesBuf[intSize+2]) {
   */
 int main(void)
 {
-    /* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-    /* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+  /* USER CODE END Init */
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-    /* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_I2C1_Init();
-    MX_USART1_UART_Init();
-    /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
 
     while (1) {
         HAL_Delay(1000);
@@ -646,9 +647,10 @@ int main(void)
 
             fprintf(diag, "\n");
 
-    /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 
     /* USER CODE END WHILE */
 
